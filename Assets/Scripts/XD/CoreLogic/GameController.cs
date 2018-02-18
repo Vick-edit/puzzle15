532bb68f2af82f4c2d5e8242fde79aebf53cf7f6
Viktor.Mironov <va.mironov@esbt.ru>
2018-02-18 23:32:44 +0500

    Добавлена логика установки и завершения игры.

--- Assets/Scripts/XD/CoreLogic/GameController.cs
+++ Assets/Scripts/XD/CoreLogic/GameController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
 using UnityEngine;
+using Random = System.Random;
 
 namespace XD.CoreLogic
 {
@@ -13,9 +16,13 @@ namespace XD.CoreLogic
         /// <summary> Константа - размерность виртуального поля </summary>
         private const int                       VIRTUAL_FIELD_SIZE = 4;
         /// <summary> Виртуальное поле пазлов </summary>
-        private AbstractPuzzleTileFacade[,]     VirtualFieldOfPuzzles;
-
-        private bool isLocked;
+        private AbstractPuzzleTileFacade[,]     virtualFieldOfPuzzles;
+        /// <summary> Семафор, многопоточного доступа к флагу блокировки </summary>
+        private readonly ReaderWriterLockSlim   virtualFieldLock = new ReaderWriterLockSlim();
+        /// <summary> флаг, указывающий заблокирована ли обработка новых игровых событий </summary>
+        private bool                            isLocked = false;
+        /// <summary> флаг, указывающий завершена ли игра </summary>
+        private bool                            isWin = false;
 
         #region Unitty
         public void OnValidate()
@@ -30,76 +37,212 @@ namespace XD.CoreLogic
 
         public void Start()
         {
-            VirtualFieldOfPuzzles = new AbstractPuzzleTileFacade[VIRTUAL_FIELD_SIZE, VIRTUAL_FIELD_SIZE];
+            FillVirtualField();
+            do
+            {
+                ShuffleVirtualField();
+                CheckIsItWin();
+            } while (isWin);
+        }
+
+        #endregion
+
+        /// <summary> Заполнить виртуальное поле пазлов и инициализировать виртуальные координаты пазлов </summary>
+        private void FillVirtualField()
+        {
+            virtualFieldOfPuzzles = new AbstractPuzzleTileFacade[VIRTUAL_FIELD_SIZE, VIRTUAL_FIELD_SIZE];
             for (int i = 0; i < VIRTUAL_FIELD_SIZE; i++)
             {
                 for (int j = 0; j < VIRTUAL_FIELD_SIZE; j++)
                 {
                     int rowNum = i;
                     int columnNum = j;
-                    int flatIndex = rowNum * 4 + columnNum;
-                    VirtualFieldOfPuzzles[i, j] = AllPuzzleTiles[flatIndex];
-                    VirtualFieldOfPuzzles[i, j].StartCoordinates = new PuzzleVirtualCoordinates(rowNum, columnNum);
-                    VirtualFieldOfPuzzles[i, j].CurrentCoordinates = new PuzzleVirtualCoordinates(rowNum, columnNum);
-                    if (!VirtualFieldOfPuzzles[i, j].IsEpmty)
+                    //Переводим координаты строки и столбца в индекс массива элементов
+                    int flatIndex = rowNum*4 + columnNum;
+                    //Иницализируем виртуальные координаты
+                    AbstractPuzzleTileFacade currentPuzzleTile = AllPuzzleTiles[flatIndex];
+                    currentPuzzleTile.StartCoordinates = new PuzzleVirtualCoordinates(rowNum, columnNum);
+                    currentPuzzleTile.CurrentCoordinates = currentPuzzleTile.StartCoordinates.Clone();
+                    //Сохраняем пазл на виртуальное поле
+                    virtualFieldOfPuzzles[rowNum, columnNum] = currentPuzzleTile;
+                    //Если текущий пазл не пустой
+                    if (!currentPuzzleTile.IsEpmty)
                     {
-                        VirtualFieldOfPuzzles[i, j].ClickСontroller.OnClicked += PuzzleWasClicked;
+                        //Привязываем обработку нажатия на пазл
+                        currentPuzzleTile.ClickСontroller.OnClicked += PuzzleWasClicked;
                     }
                 }
             }
         }
-        #endregion
 
+        /// <summary> Перемешать пазлы </summary>
+        private void ShuffleVirtualField()
+        {
+            int maxPuzzleIndex = PUZZLES_COUNT;
+
+            //На последнем месте на виртуальном поле всегда должен стоять пустой пазл
+            AbstractPuzzleTileFacade lastPuzzleTile = virtualFieldOfPuzzles[VIRTUAL_FIELD_SIZE - 1, VIRTUAL_FIELD_SIZE - 1];
+            if (!lastPuzzleTile.IsEpmty)
+            {
+                AbstractPuzzleTileFacade emptyPuzzleTile = AllPuzzleTiles.First(puzzle => puzzle.IsEpmty);
+                SwapToPuzzles(lastPuzzleTile, emptyPuzzleTile);
+            }
+            //В одномерном списке пазлов пустой так же должен стоять на последнем месте
+            lastPuzzleTile = AllPuzzleTiles.Last();
+            if (!lastPuzzleTile.IsEpmty)
+            {
+                for (int i = 0; i < PUZZLES_COUNT; i++)
+                {
+                    if (AllPuzzleTiles[i].IsEpmty)
+                    {
+                        AllPuzzleTiles[PUZZLES_COUNT - 1] = AllPuzzleTiles[i];
+                        AllPuzzleTiles[i] = lastPuzzleTile;
+                        break;
+                    }
+                }
+            }
+            //т.к. последний пазл пустой, его мы трогать не будем - уменьшаем максимальный индекс элементов, которые будем перемешивать
+            maxPuzzleIndex--;
+
+            Random random = new Random();
+            //для всех оставшихся пазлов - 1
+            for (int oldIndex = 0; oldIndex < maxPuzzleIndex-1; oldIndex++)
+            {
+                //определяем новый индекс, больший, чем текущий
+                int newIndex = random.Next(oldIndex + 1, maxPuzzleIndex);
+
+                //меняем местами, соответствующие элементы пазла
+                SwapToPuzzles(AllPuzzleTiles[oldIndex], AllPuzzleTiles[newIndex]);
+            }
+        }
+
+        /// <summary> Обмен местами на виртуальной и визуальной доске двух пазлов </summary>
+        private void SwapToPuzzles(AbstractPuzzleTileFacade firstPuzzle, AbstractPuzzleTileFacade secondPuzzle)
+        {
+            Vector3 firstPuzzleVector = firstPuzzle.transform.position;
+            PuzzleVirtualCoordinates firstPuzzleCoordinates = firstPuzzle.CurrentCoordinates;
+
+            firstPuzzle.transform.position = secondPuzzle.transform.position;
+            firstPuzzle.CurrentCoordinates = secondPuzzle.CurrentCoordinates;
+
+            secondPuzzle.transform.position = firstPuzzleVector;
+            secondPuzzle.CurrentCoordinates = firstPuzzleCoordinates;
+
+            virtualFieldOfPuzzles[GetPuzzleRow(firstPuzzle), GetPuzzleCol(firstPuzzle)] = firstPuzzle;
+            virtualFieldOfPuzzles[GetPuzzleRow(secondPuzzle), GetPuzzleCol(secondPuzzle)] = secondPuzzle;
+        }
+
+        /// <summary> Обработка нажатия на пазл </summary>
+        /// <param name="puzzleTile">Пазл, по которому произошло нажатие</param>
         private void PuzzleWasClicked(AbstractPuzzleTileFacade puzzleTile)
         {
+            virtualFieldLock.EnterReadLock();
+            //Если состояние игры - заблокирована
             if (isLocked)
             {
+                //То не обрабатываем нажатия на пазлы
+                virtualFieldLock.ExitReadLock();
+                return;
+            }
+            virtualFieldLock.ExitReadLock();
+
+            var emptyPuzzleTile = GetEmptyNeighbor(puzzleTile);
+            //Если среди соседних пазлов нет пустого
+            if (emptyPuzzleTile == null)
+            {
+                //Завершаем обработку нажатия
                 return;
             }
 
+            //Иначе входим в цикл обмена местами нажатого пазла и соседнего пустого
+            virtualFieldLock.EnterWriteLock();
             isLocked = true;
+            virtualFieldLock.ExitWriteLock();
 
-            int rowNum = puzzleTile.CurrentCoordinates.Row;
-            int columnNum = puzzleTile.CurrentCoordinates.Column;
+            //Привязываем обработчик завершения обмена местами пазлов
+            puzzleTile.OnSwapEndedEvent += PuzzlesSwaped;
+            //Начинаем обмен местами
+            puzzleTile.SwapWithEmptyTile(emptyPuzzleTile);
+        }
 
-            int topNeighborRow = rowNum -1;
-            int bottomNeighborRow = rowNum + 1;
-            int leftNeighborCol = columnNum - 1;
-            int rightNeighborCol = columnNum + 1;
+        /// <summary> Обработка завершения обмена местами пазлов по нажатию на один из них </summary>
+        private void PuzzlesSwaped(AbstractPuzzleTileFacade notEmptyPuzzle, AbstractPuzzleTileFacade emptyPuzzle)
+        {
+            try
+            {
+                //обновляем координаты на виртуальном поле
+                virtualFieldOfPuzzles[GetPuzzleRow(notEmptyPuzzle), GetPuzzleCol(notEmptyPuzzle)] = notEmptyPuzzle;
+                virtualFieldOfPuzzles[GetPuzzleRow(emptyPuzzle), GetPuzzleCol(emptyPuzzle)] = emptyPuzzle;
+                CheckIsItWin();
+            }
+            finally
+            {
+                //высвобождаем флаг
+                virtualFieldLock.EnterWriteLock();
+                isLocked = isWin; //isLocked = false, только если не была завершена игра этим ходом
+                virtualFieldLock.ExitWriteLock();
+            }
+        }
+
+        /// <summary> Найти пустой пазл среди соседних к текущему пазлу </summary>
+        /// <param name="puzzleTile">Пазл для которого осуществляем поиск</param>
+        /// <returns>Пустой соседний пазл или null</returns>
+        private AbstractPuzzleTileFacade GetEmptyNeighbor(AbstractPuzzleTileFacade puzzleTile)
+        {
+            int currentRow = puzzleTile.CurrentCoordinates.Row;
+            int currentCol = puzzleTile.CurrentCoordinates.Column;
+
+            int topNeighborRow = currentRow - 1;
+            int bottomNeighborRow = currentRow + 1;
+            int leftNeighborCol = currentCol - 1;
+            int rightNeighborCol = currentCol + 1;
 
             AbstractPuzzleTileFacade emptyPuzzleTile = null;
 
-            if (topNeighborRow >= 0 && VirtualFieldOfPuzzles[topNeighborRow, columnNum].IsEpmty)
+            if (topNeighborRow >= 0 && virtualFieldOfPuzzles[topNeighborRow, currentCol].IsEpmty)
             {
-                emptyPuzzleTile = VirtualFieldOfPuzzles[topNeighborRow, columnNum];
+                emptyPuzzleTile = virtualFieldOfPuzzles[topNeighborRow, currentCol];
             }
-            else if (bottomNeighborRow < VIRTUAL_FIELD_SIZE && VirtualFieldOfPuzzles[bottomNeighborRow, columnNum].IsEpmty)
+            else if (bottomNeighborRow < VIRTUAL_FIELD_SIZE && virtualFieldOfPuzzles[bottomNeighborRow, currentCol].IsEpmty)
             {
-                emptyPuzzleTile = VirtualFieldOfPuzzles[bottomNeighborRow, columnNum];
+                emptyPuzzleTile = virtualFieldOfPuzzles[bottomNeighborRow, currentCol];
             }
-            else if (leftNeighborCol >=0 && VirtualFieldOfPuzzles[rowNum, leftNeighborCol].IsEpmty)
+            else if (leftNeighborCol >= 0 && virtualFieldOfPuzzles[currentRow, leftNeighborCol].IsEpmty)
             {
-                emptyPuzzleTile = VirtualFieldOfPuzzles[rowNum, leftNeighborCol];
+                emptyPuzzleTile = virtualFieldOfPuzzles[currentRow, leftNeighborCol];
             }
-            else if (rightNeighborCol < VIRTUAL_FIELD_SIZE && VirtualFieldOfPuzzles[rowNum, rightNeighborCol].IsEpmty)
+            else if (rightNeighborCol < VIRTUAL_FIELD_SIZE && virtualFieldOfPuzzles[currentRow, rightNeighborCol].IsEpmty)
             {
-                emptyPuzzleTile = VirtualFieldOfPuzzles[rowNum, rightNeighborCol];
+                emptyPuzzleTile = virtualFieldOfPuzzles[currentRow, rightNeighborCol];
             }
 
-            if (emptyPuzzleTile != null)
+            return emptyPuzzleTile;
+        }
+
+        /// <summary> Проверить завершена ли игра </summary>
+        private void CheckIsItWin()
+        {
+            isWin = true;
+            foreach (var puzzle in AllPuzzleTiles)
             {
-                puzzleTile.SwapWithEmptyTile(emptyPuzzleTile);
-                puzzleTile.OnSwapEndedEvent += (notEmptyPuzzle, emptyPuzzle) => 
+                if (!puzzle.CurrentCoordinates.Equals(puzzle.StartCoordinates))
                 {
-                    VirtualFieldOfPuzzles[notEmptyPuzzle.CurrentCoordinates.Row, notEmptyPuzzle.CurrentCoordinates.Column] = notEmptyPuzzle;
-                    VirtualFieldOfPuzzles[emptyPuzzle.CurrentCoordinates.Row, emptyPuzzle.CurrentCoordinates.Column] = emptyPuzzle;
-                    isLocked = false;
-                };
+                    isWin = false;
+                    break;
+                }
             }
-            else
-            {
-                isLocked = false;
-            }
+        }
+
+        /// <summary> Номер строки из текущей координаты пазла </summary>
+        private int GetPuzzleRow(AbstractPuzzleTileFacade puzzleTile)
+        {
+            return puzzleTile.CurrentCoordinates.Row;
+        }
+
+        /// <summary> Номер столбца из текущей координаты пазла </summary>
+        private int GetPuzzleCol(AbstractPuzzleTileFacade puzzleTile)
+        {
+            return puzzleTile.CurrentCoordinates.Column;
         }
     }
 }
